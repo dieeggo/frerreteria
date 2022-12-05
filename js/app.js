7c5cc453d029390081cbd36390439fd12574c71c
dieeggo <diego.mulato1101@gmail.com>
2022-12-05 08:43:01 -0600

    septimo commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
-//Se define que el sw.js se encuentra
-//en el repositorio.
-var ubicacionSw='/frerreteria/sw2.js';
-
 if ( navigator.serviceWorker ) {
-    if(url.includes('localhost')){
-        ubicacionSw='/sw.js';
-    }
-
-    navigator.serviceWorker.register(ubicacionSw);
+    
+navigator.serviceWorker.register('/sw2.js');
 
 }
